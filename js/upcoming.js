a9462fedda0ca5512469bfaf5f611cc6833fc6e1
Kamil Szczesniak <kamil@Kamils-MacBook-Pro.local>
2019-12-07 12:59:34 -0600

    commit

---
+++ js/upcoming.js
@@ -0,0 +1,15 @@
+
+$(document).ready(() => {
+    var url = getUrl();
+    getMovies(url);
+});
+
+function getUrl() {
+  let url = "https://api.themoviedb.org/3/movie/upcoming?";
+  var params = $.param({
+    api_key: apiKey
+  });
+
+  url += params;
+  return url;
+}
